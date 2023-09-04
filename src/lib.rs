7016b2eae38837a4bdd67e384987145d8c4ca1ca
Olivier Beguin <olivier.beguinti@gmail.com>
2023-09-04 17:07:05 -0400

    started chunks extraction

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,9 @@ use std::fs::write;
 mod args;
 pub use args::Args;
 
+mod png;
+pub use png::Png;
+
 pub fn run(args: Args) -> Result<(), Error> {
 
     let in_file = File::open(args.in_file_path)?;
@@ -15,7 +18,14 @@ pub fn run(args: Args) -> Result<(), Error> {
     let mut buffer = Vec::new();
 
     reader.read_to_end(&mut buffer)?;
-    write(args.out_file_path, buffer)?;
+
+    let png = Png::build(&buffer).unwrap();
+    println!("{} chunks", png.chunks.len());
+    for chunk in png.chunks {
+        println!("{}", chunk.chunk_type);
+    }
+
+    //write(args.out_file_path, buffer)?;
 
     Ok(())
 }
